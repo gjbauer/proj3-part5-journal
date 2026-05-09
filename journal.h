df63017ae090abdef98a53a68f2828343febcf41
Gabriel Bauer <gabeb1277@gmail.com>
2026-05-08 22:37:20 -0400

    some journal engineering improvements

--- journal.h
+++ journal.h
@@ -55,9 +55,9 @@ typedef struct journal_header_t
 
 void set_journal_head(DiskInterface *disk, cache *cache, int head_position);
 
-void initialize_journal_entry(DiskInterface *disk, cache *cache, transaction_type_t type);
+void initialize_journal_entry(DiskInterface *disk, cache *cache, journal_entry_t *entry);
 
-void sync_entry(DiskInterface *disk, cache *cache, journal_entry_t entry);
+void sync_entry(DiskInterface *disk, cache *cache, journal_entry_t *entry);
 
 void sync_journal(DiskInterface *disk, cache *cache);
 
