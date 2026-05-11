b6b29a2ed0f191b78133d910306c04500ffcbb8a
Gabriel Bauer <gabeb1277@gmail.com>
2026-05-11 08:56:55 -0400

    added function calls to switch statement in journal.c

--- journal.c
+++ journal.c
@@ -1,5 +1,6 @@
 #include "superblock.h"
 #include "journal.h"
+#include "metadata-api.h"
 
 void set_journal_head(DiskInterface *disk, cache *cache, int head_position);
 
@@ -12,14 +13,19 @@ void sync_entry(DiskInterface *disk, cache *cache, journal_entry_t *entry)
     switch (entry->type)
     {
         case MKNOD:
+            _mknod(disk, cache, entry->mknod.path, entry->mknod.mode, true);
             break;
         case UNLINK:
+            _unlink(disk, cache, entry->unlink.path, true);
             break;
         case LINK:
+            _link(disk, cache, entry->link.from, entry->link.to, true);
             break;
         case CHMOD:
+            _chmod(disk, cache, entry->chmod.path, entry->chmod.mode, true);
             break;
         case TRUNCATE:
+            _truncate(disk, cache, entry->truncate.path, entry->truncate.size, true);
             break;
     }
 }
